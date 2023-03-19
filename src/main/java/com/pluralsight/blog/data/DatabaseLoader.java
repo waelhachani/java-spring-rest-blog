c015e4899d97932a60cfc22da501df74584fa154
waelhachani <med.wael.hachani@gmail.com>
2023-03-19 12:27:12 -0400

    code correction

--- src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
+++ src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
@@ -14,7 +14,7 @@ import java.util.stream.IntStream;
 
 @Component
 public class DatabaseLoader implements ApplicationRunner {
-    @Autowired
+
     private final PostRepository postRepository;
     private final String[] templates = {
             "Smart Home %s", "Mobile %s - For When You're On he Go", "The %s - Your New Favorite Accessory"};
@@ -22,7 +22,7 @@ public class DatabaseLoader implements ApplicationRunner {
             "Earbuds", "Speakers", "Tripod", "Instant Pot", "Coffee Cup", "Keyboard", "Sunglasses"};
     public List<Post> randomPosts = new ArrayList<>();
     public List<Author> authors = new ArrayList<>();
-
+    @Autowired
     public DatabaseLoader(PostRepository postRepository) {
         this.postRepository = postRepository;
     }
